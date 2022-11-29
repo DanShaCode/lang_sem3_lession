3b42889ce4863a008ec8e899a53ccb64d260ef98
Danis <danshadance@mail.ru>
2022-11-29 23:40:09 +0300

    Начало решения задачи 17

---
+++ task2/task2.cs
@@ -0,0 +1,12 @@
+﻿// Принимает на вход координаты точки X и Y
+// X != 0 && Y != 0;
+// Выдаем четверть плоскости, в которой находится эта точка
+
+Console.WriteLine("Введите X:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Кооридината X: " + x);
+Console.WriteLine("Координата Y: " + y);
