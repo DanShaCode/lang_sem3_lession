2087cd6f25350425137d3323f0689660bbd2b187
Danis <danshadance@mail.ru>
2022-11-30 10:04:08 +0300

    Задача 17 решена

--- task2/task2.cs
+++ task2/task2.cs
@@ -8,5 +8,29 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите Y:");
 int y = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Кооридината X: " + x);
-Console.WriteLine("Координата Y: " + y);
+if (x == 0 | y == 0)
+{
+    Console.WriteLine("Ошибка ввода: X или Y не может быть равен 0");
+}
+else
+{
+    if (x > 0 | y > 0)
+{
+    Console.WriteLine("Координата находится в ПЕРВОЙ четверти");
+}
+
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("Координата находится во ВТОРОЙ четверти");
+}
+
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("Коориданата находится в ТРЕТЬЕЙ четверти");
+}
+
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("Координата находится в ЧЕТВЕРТОЙ четверти");
+}
+}
