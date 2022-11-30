f4068b3c5384a413eb2d7f014e7e87817e000a60
Danis <danshadance@mail.ru>
2022-11-30 10:37:40 +0300

    Мучаюсь над 18 задачей

---
+++ task3/task3.cs
@@ -0,0 +1,23 @@
+﻿// Указываем номер четверти
+// Показывает диапозон возможных координат точек в этой четверти
+
+Console.WriteLine("Введите номер четверти:");
+int data = Convert.ToInt32(Console.ReadLine());
+int quarter = data;
+
+int[] q1x = {1,2,3};
+q1x[0] = 1;
+q1x[1] = 2;
+q1x[2] = 3;
+
+int[] q1y = {1,2,3};
+q1y[0] = 1;
+q1y[1] = 2;
+q1y[2] = 3;
+
+if (quarter == 1)
+{
+    Console.Write(q1x[0]);
+    Console.Write(q1x[1]);
+    Console.Write(q1y[0]);
+}
